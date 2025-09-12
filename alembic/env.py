4b78f649d81b454f35fa77f03c5d133e9884cd55
Gabbzgreat <mikendlovu771@gmail.com>
2025-09-12 04:42:16 +0200

    update alembic config with new password

--- alembic/env.py
+++ alembic/env.py
@@ -15,7 +15,7 @@ password = urllib.parse.quote_plus(settings.database_password)
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option("sqlalchemy.url",f'postgresql://{settings.database_username}:{password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
+config.set_main_option("sqlalchemy.url",f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
 )
 
 # Interpret the config file for Python logging.
